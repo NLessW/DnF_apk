3ec9a4ee5ba22bceb83fb7c53f015d2b3d051e99
Less <63160418+NLessW@users.noreply.github.com>
2024-01-11 01:53:49 +0900

    Update MainActivity.java
    Updated selected server memory.

--- Java_Code/MainActivity.java
+++ Java_Code/MainActivity.java
@@ -1,3 +1,5 @@
+package com.cookandroid.dnf;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -15,13 +17,12 @@ public class MainActivity extends AppCompatActivity {
     EditText nick;
     Button search;
     String ns, sChoice;
+    String previousChoice = "cain"; // 이전 선택값을 저장하는 변수
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
         server = findViewById(R.id.server);
         final String[] serverList = {"카인", "디레지에", "시로코", "프레이", "카시야스", "힐더", "안톤", "바칼"};
@@ -34,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         server.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                sChoice = serverList[position]; // 선택한 서버 값을 sChoice 변수에 할당
+                sChoice = serverList[position];
             }
 
             @Override
@@ -50,41 +51,36 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, characterInfo.class);
                 intent.putExtra("nickName", ns);
 
-                // 서버 선택에 따라 값을 설정
-                switch (sChoice) {
-                    case "카인":
-                        sChoice = "cain";
-                        break;
-                    case "디레지에":
-                        sChoice = "diregie";
-                        break;
-                    case "시로코":
-                        sChoice = "siroco";
-                        break;
-                    case "프레이":
-                        sChoice = "prey";
-                        break;
-                    case "카시야스":
-                        sChoice = "casillas";
-                        break;
-                    case "힐더":
-                        sChoice = "hilder";
-                        break;
-                    case "안톤":
-                        sChoice = "anton";
-                        break;
-                    case "바칼":
-                        sChoice = "bakal";
-                        break;
-                    default:
-                        sChoice = "카인";  // 기본값 설정
-                        break;
-                }
+                // 이전 선택값을 업데이트
+                previousChoice = convertToEnglish(sChoice);
 
-                intent.putExtra("serverSelect", sChoice);
+                intent.putExtra("serverSelect", previousChoice);
                 startActivity(intent);
-
             }
         });
+    }
+
+    // 서버 이름을 영문으로 변환하는 메서드
+    private String convertToEnglish(String sChoice) {
+        switch (sChoice) {
+            case "카인":
+                return "cain";
+            case "디레지에":
+                return "diregie";
+            case "시로코":
+                return "siroco";
+            case "프레이":
+                return "prey";
+            case "카시야스":
+                return "casillas";
+            case "힐더":
+                return "hilder";
+            case "안톤":
+                return "anton";
+            case "바칼":
+                return "bakal";
+            default:
+                return "cain"; // 기본값 설정
+        }
     }
 }
